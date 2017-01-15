e40ac2c6f78d1acb5a9110f46439007c73c5627c
roinflames <rod.reyes.s@gmail.com>
2017-01-15 17:30:04 -0300

    cambios para index

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ var isAuthenticated = function (req, res, next) {
 
 module.exports = function(passport){
 	/* GET Home Page */
-	router.get('/home', function(req, res){
+	router.get('/home', isAuthenticated, function(req, res){
 
 		//var port = new SerialPort('/dev/ttyUSB0');
 		//port.on('data', function (data) {
@@ -78,25 +78,29 @@ module.exports = function(passport){
 	});
 
 /* GET estado*/
-	router.get('/estado', function(req, res) {
+	router.get('/estado', isAuthenticated,  function(req, res) {
   	res.render('estado', { Lunes: arduino.dia.medicion.Lunes, Martes: arduino.dia.medicion.Martes, Miercoles:arduino.dia.medicion.Miercoles, Jueves: arduino.dia.medicion.Jueves, Viernes: arduino.dia.medicion.Viernes, Sabado: arduino.dia.medicion.Sabado, Domingo: arduino.dia.medicion.Domingo});
 	});
 
-	router.get('/historico', function(req, res) {
+	router.get('/historico', isAuthenticated, function(req, res) {
   	res.render('historico');
 	});
 
-	router.get('/estacion', function(req, res) {
+	router.get('/estacion', isAuthenticated, function(req, res) {
   	//res.render('estacion');
 		var dato = "sample";
 		res.render('estacion', {lectura: dato});
 	});
 
-	router.get('/perfil', function(req, res){
+	router.get('/perfil', isAuthenticated, function(req, res){
 		res.render('perfil', { user: req.user });
 	});
 
-	router.get('/arduino', function(req, res){
+	router.get('/ingresar', isAuthenticated, function(req, res){
+		res.render('ingresar');
+	});
+
+	router.get('/arduino', isAuthenticated, function(req, res){
 		res.render('arduino', { title: 'Hey', message: 'you'});
 		console.log("arduino");
 	});
